aa10802b1014056ef0c408f2fe57984821e72863
Alan Donohoe <alandonohoe123@gmail.com>
2013-09-19 08:27:42 +0100

    date based slider communicating with data

--- habitat_loss/js/slider.js
+++ habitat_loss/js/slider.js
@@ -1,115 +1,133 @@
 //slider.js
-
 //TODO: get the JS out of main html file and wrap it in here.
 var sliderObj = {
-    leapFrame: null,
-    slider: null,
-    handle: null,
 
-    margin: {
+   // var leapFrame;
+    delayReset: 50, // increase to reduce leap motion "frame rate"
+    delay: this.delayReset,
+    gBrush: null,
+    width:960 - 40 - 40,
+    height: 500 - 200 - 200,
+
+    //margin: {
         top: 200,
-        right: 50,
+        right: 40,
         bottom: 200,
-        left: 50,
-    },
+        left: 40,
+   // },
 
-    width: 960 - this.margin.left - this.margin.right,
-    height: 500 - this.margin.bottom - this.margin.top,
+    brushended : function() {
+        if (delay-- < 0) { // need to reduce leap motion "frame rate"
+            delay = delayReset;
+            //chart.drawHomelessData(counter++);
+            // if (counter > (chart.quarters2.length -1)) { // hardcoded length of homeless data: 8 here
+            //     counter = 0;
+            // }
+        }
+        if (!d3.event.sourceEvent) return; // only transition after input
+        var extent0 = brush.extent(),
+            extent1 = extent0.map(d3.time.month.round);
 
+        // if empty when rounded, use floor & ceil instead
+        if (extent1[0] >= extent1[1]) {
+            extent1[0] = d3.time.month.floor(extent0[0]);
+            extent1[1] = d3.time.month.ceil(extent0[1]);
+        }
 
-    x: d3.scale.linear().domain([0, 180]).range([0, width]).clamp(true),
-
-    brush: d3.svg.brush().x(x).extent([0, 0]).on("brush", brushed),
-
-    svg: d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-
-    init: function () {
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height / 2 + ")")
-            .call(d3.svg.axis()
-                .scale(x)
-                .orient("bottom")
-                .tickFormat(function (d) {
-                    return d + "°";
-                })
-                .tickSize(0)
-                .tickPadding(12))
-            .select(".domain")
-            .select(function () {
-                return this.parentNode.appendChild(this.cloneNode(true));
-            })
-            .attr("class", "halo")
-
-
-        slider: svg.append("g")
-            .attr("class", "slider")
-            .call(brush);
-
-        slider.selectAll(".extent,.resize")
-            .remove();
-
-        slider.select(".background")
-            .attr("height", height);
-
-        handle: slider.append("circle")
-            .attr("class", "handle")
-            .attr("transform", "translate(0," + height / 2 + ")")
-            .attr("r", 9),
-
-        slider
-            .call(brush.event)
-            .transition() // gratuitous intro!
-        .duration(750)
-            .call(brush.extent([70, 70]))
+        d3.select(this).transition()
+            .call(brush.extent(extent1))
             .call(brush.event);
     },
 
 
+    x:  d3.time.scale()
+        .domain([new Date(2010, 12, 1), new Date(2013, 6, 1)])
+        .range([0, this.width]),
 
-    brushed: function () {
-        var value = brush.extent()[0];
+    brush: d3.svg.brush()
+        .x(this.x)
+        .extent([new Date(2010, 12, 1), new Date(2011, 2, 1)])
+        .on("brushend", this.brushended),
 
-        //if (d3.event.sourceEvent) { // not a programmatic event
-        //  value = x.invert(d3.mouse(this)[0]);
-        //  brush.extent([value, value]);
-        //}
-        if (leapFrame) { // not a programmatic event
-            if (leapFrame.pointables[0]) {
-                value = leapFrame.pointables[0].tipPosition[0];
-                brush.extent([value, value]);
+    svg: d3.select("body").append("svg")
+        .attr("width", this.width + this.left + this.right)
+        .attr("height", this.height + this.top + this.bottom)
+        .append("g")
+        .attr("transform", "translate(" + this.left + "," + this.top + ")"),
+
+    init : function(){
+
+        // not referencing margin:
+        // width: 960 - this.margin.left - this.margin.right,
+        // height: 500 - this.margin.top - this.margin.bottom,
+        this.width = 960 - 40 - 40;
+        this.height = 500 - 200 - 200;
+
+        svg.append("rect")
+        .attr("class", "grid-background")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.append("g")
+        .attr("class", "x grid")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .ticks(d3.time.month, 3)
+            .tickSize(-height)
+            .tickFormat(""))
+        .selectAll(".tick")
+        .classed("minor", function (d) {
+            return d.getHours();
+        });
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .ticks(d3.time.month, 3)
+            .tickPadding(0))
+        .selectAll("text")
+        .attr("x", 6)
+        .style("text-anchor", null);
+
+    gBrush: svg.append("g")
+        .attr("class", "brush")
+        .call(brush)
+        .call(brush.event);
+
+    gBrush.selectAll("rect")
+        .attr("height", height);
+
+    },    
+
+   
+
+    updateSlider: function(leapPoint) {
+
+        var curSlideL = x(brush.extent()[0]);
+        var curSlideR = x(brush.extent()[1]);
+
+        // if its -ve - move backwards if its +ve - move fwds:
+        if (leapPoint > 0) {
+            if (curSlideR > width) { // at end of slide.
+                return;
+            } else {
+                curSlideL = curSlideL + 5;
+                curSlideR = curSlideR + 5;
+            }
+        } else {
+            if (curSlideL < 0) { // at beginning of slide
+                return;
+            } else {
+                curSlideL = curSlideL - 5;
+                curSlideR = curSlideR - 5;
             }
         }
-
-        handle.attr("cx", x(value));
-        d3.select("body").style("background-color", d3.hsl(value, .8, .8));
+        gBrush.call(brush.extent([x.invert(curSlideL), x.invert(curSlideR)]))
+            .call(brush.event);
     },
-
-    leapMotionListener : function(frame){
-    	this.leapFrame=frame;
-    	if (this.leapFrame) { // not a programmatic event
-        	if(this.leapFrame.pointables[0]){
-        		sliderObj.slider
-        		.call(brush.event)
-        		.call(brush.extent(frame.pointables[0].tipPosition[0]))
-        		.call(brush.event);
-        	}
-        }
-    },
-};
-
-Leap.loop(function (frame) {
-    sliderObj.leapFrame = frame;
-    if (leapFrame) { // not a programmatic event
-        if (leapFrame.pointables[0]) {
-            sliderObj.slider
-                .call(brush.event)
-                .call(brush.extent(frame.pointables[0].tipPosition[0]))
-                .call(brush.event);
-        }
-    }
-});
+}
