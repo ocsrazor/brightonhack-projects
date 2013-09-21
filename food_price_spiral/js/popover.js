8d8d5cdbc2988e36348666f508c7596fc8f3de13
riccardo.tonini <riccardotonini@gmail.com>
2013-09-21 13:21:35 +0100

    Adjusting position od pop up

--- food_price_spiral/js/popover.js
+++ food_price_spiral/js/popover.js
@@ -11,7 +11,7 @@ function showPopOver(divID, year) {
     popUpDiv.style.width = 800 + 'px';
     popUpDiv.style.height = 500 + 'px';
     var left = (window.innerWidth / 2) - 400 + "px"
-    var top = (window.innerHeight / 2) - 350 + "px";
+    var top = (window.innerHeight / 2) - 250 + "px";
     popUpDiv.style.left = left;
     popUpDiv.style.top = top;
     popUpDiv.innerHTML = '<h1>' + year + '</h1><div id="chartZoom"><a href="#" id="zoomIn">Zoom In</a> <a href="#" id="zoomOut">Zoom Out</a><a href="javascript:closePopOver(\'pop1\');" title="Close window">Close me</a></div><div id="chart1" class="with-transitions"><svg></svg></div>';
@@ -20,7 +20,7 @@ function showPopOver(divID, year) {
     // SHOW THE DIV
     popUpDiv.style.display = "block";
 }
-/** 
+/**
  * Closes line chart popup
  * @param {String} divID
  * @returns {undefined}
